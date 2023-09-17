cdae59fca3b5f0a97125e6077e628de56e92f12a
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-09-17 19:26:55 +0900

    230917_회원 도메인과 리포지토리 작성

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;  //sequence => key 값을 생성해주는 것이라고 생각하면 됨
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);  //sequence 값을 하나 올려주고 id값 세팅
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
